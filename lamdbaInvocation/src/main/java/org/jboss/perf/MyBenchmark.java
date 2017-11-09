896deae1add7b1cf409a271f6bfe3fa6a9d93f5c
johara <johara@redhat.com>
2017-11-09 20:31:29 +0000

    modified benchmark to remove MH validation on each invocation

--- lamdbaInvocation/src/main/java/org/jboss/perf/MyBenchmark.java
+++ lamdbaInvocation/src/main/java/org/jboss/perf/MyBenchmark.java
@@ -7,6 +7,7 @@ import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
 
 import java.lang.invoke.CallSite;
+import java.lang.invoke.LambdaConversionException;
 import java.lang.invoke.LambdaMetafactory;
 import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
@@ -23,30 +24,50 @@ public class MyBenchmark {
       public SimpleBean simpleBean;
 
       public int count = 0;
-      public Method reflected;
-      public MethodHandles.Lookup lookup;
-      public MethodHandle mh;
-      public IntBinaryOperator lambda;
+      public static final Method reflected;
+      public static final MethodHandles.Lookup lookup;
+      public static final MethodHandle mh;
+      public static final IntBinaryOperator lambda;
+
+      static {
+         MethodHandle initializeMH = null;
+         Method initializeReflected = null;
+         IntBinaryOperator reflectedLambda = null;
+
+         lookup = MethodHandles.lookup();
+
+         try {
+            initializeReflected = SimpleBean.class.getDeclaredMethod( "myMethod", int.class, int.class );
+            initializeMH = lookup.unreflect( initializeReflected );
+
+            CallSite site = LambdaMetafactory.metafactory(
+                lookup, "applyAsInt", MethodType.methodType( IntBinaryOperator.class ),
+                initializeMH.type(), initializeMH, initializeMH.type() );
+
+            MethodHandle factory = site.getTarget();
+
+            reflectedLambda = (IntBinaryOperator) factory.invokeExact();
+
+
+         } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+         } catch (IllegalAccessException e) {
+            e.printStackTrace();
+         } catch (LambdaConversionException e) {
+            e.printStackTrace();
+         } catch (Throwable throwable) {
+            throwable.printStackTrace();
+         }
+
+         lambda = reflectedLambda;
+         reflected = initializeReflected;
+         mh = initializeMH;
+      }
 
       @Setup(Level.Trial)
       public void setup() throws Throwable {
 
          simpleBean = new SimpleBean();
-
-         lookup = MethodHandles.lookup();
-
-         reflected = SimpleBean.class.getDeclaredMethod( "myMethod", int.class, int.class );
-         MethodHandle target = lookup.unreflect( reflected );
-
-         mh = target;
-
-         CallSite site = LambdaMetafactory.metafactory(
-            lookup, "applyAsInt", MethodType.methodType( IntBinaryOperator.class ),
-            mh.type(), mh, mh.type() );
-
-         MethodHandle factory = site.getTarget();
-
-         lambda = (IntBinaryOperator) factory.invokeExact();
 
       }
    }
